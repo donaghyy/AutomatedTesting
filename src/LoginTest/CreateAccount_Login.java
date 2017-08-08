268d81030cf6d12c45474fe5096101841bdaee5c
David Donaghy <davidsxbl@hotmail.co.uk>
2017-08-08 16:57:56 +0100

    Reports

--- src/LoginTest/CreateAccount_Login.java
+++ src/LoginTest/CreateAccount_Login.java
@@ -9,16 +9,21 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.google.common.base.Function;
+
+import Reports.ScreenShot;
+import junit.extensions.TestSetup;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
-
-
-//import static jdk.nashorn.internal.runtime.PropertyHashMap.findElement;
 
 
 
@@ -27,9 +32,14 @@ public class CreateAccount_Login {
     private WebDriver webDriver;
     LoginPage l = new LoginPage();
     Wait<WebDriver> wait;
+    
+    private ExtentReports report;
+    private ExtentTest test;
+    private String reportFilePath = "Report.html";
+    ScreenShot snap = new ScreenShot();
 
     @Before
-    public void bTest() {
+    public void setUp() {
         System.out.println("Before");
         System.setProperty("webdriver.chrome.driver", "/Users/daviddonaghy/Desktop/Documents/QA/QA Slides"
         		+ "/Week 5 Testing/chromedriver");
@@ -43,30 +53,48 @@ public class CreateAccount_Login {
 				.ignoring(NoSuchElementException.class);
         
         
+        //______________________________________________________REPORTS
+        report = new ExtentReports();
+        ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter(reportFilePath);
+        extentHtmlReporter.config().setReportName("ReportName");
+        extentHtmlReporter.config().setDocumentTitle("DocumentTitle");
+        report.attachReporter(extentHtmlReporter);
+        test = report.createTest("TestName");
     }
 
     @After
     public void aTest() {
         System.out.println("Test Complete.");
         webDriver.quit();
+        
+        //tests.clear();
+        report.flush();
     }
 
+    
     @Test
-    public void CreateAcc_Login() throws InterruptedException {
-      
+    public void CreateAcc_Login() throws InterruptedException, IOException {
+    		
+    	
+    			test.log(Status.WARNING, "This is a warning! Used to report warnings");
+    		String imagePath = ScreenShot.take(webDriver, "TEST IMAGE");
+    	
     		System.out.println("Begin Test ...");
         webDriver.navigate().to("http://thedemosite.co.uk/login.php");
         
         l.goRegistration();
-        		//l.sleep(3);
+        
         l.enterCredentials("test", "test");
         		
         l.submitClick();
-        		
+        
+        		test.log(Status.INFO,"Info level");
+        
         l.goLogin();
+        		snap.take(webDriver, "TEST IMAGE");
         		
         l.enterCredentials("test", "test");
-        		
+        
         l.submitClick();
         
         
@@ -76,26 +104,13 @@ public class CreateAccount_Login {
         TimeUnit.SECONDS.sleep(5);
         webDriver.quit();
         
-        /*//webDriver.manage().window().maximize();
         
-        //webDriver.findElement(By.cssSelector("small > a:nth-child(6)")).click();
-        
-        webDriver.findElement(By.name("username")).sendKeys("test");
-        webDriver.findElement(By.name("password")).sendKeys("test");
-        
-        //webDriver.findElement(By.name("FormsButton2")).click();        
-        
-        //webDriver.findElement(By.cssSelector("small > a:nth-child(7)")).click();
-        
-        webDriver.findElement(By.name("username")).sendKeys("test");
-        webDriver.findElement(By.name("password")).sendKeys("test");
-        
-       // webDriver.findElement(By.name("FormsButton2")).click();        
-        
-        assertEquals("**Successful Login**", webDriver.findElement(By.
-        		cssSelector("body > table > tbody > tr > td.auto-style1 > big > blockquote > blockquote > font > center > b")).getText());
-         */
-        
+        /*WebElement UN = wait.until(new Function<WebDriver, WebElement>()
+		{
+			public WebElement apply(WebDriver driver) {
+				return driver.findElement(By.name("username"));
+			}
+		});*/
         
     }
     
