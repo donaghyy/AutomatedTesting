8a42e135c1567df752d0f0b6073589ab334b452d
David Donaghy <davidsxbl@hotmail.co.uk>
2017-08-08 13:03:19 +0100

    j

---
+++ src/LoginTest/TestRunner.java
@@ -0,0 +1,15 @@
+package LoginTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CreateAccount_Login.class
+})
+
+public class TestRunner {
+
+
+
+}
